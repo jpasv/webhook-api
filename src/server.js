cbddf8e86a36590b024fb77c268ebffea4fe9940
João Pedro <jp@asv.digital>
2023-10-29 19:33:59 -0300

    include cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketIo = require('socket.io')
 const app = express()
+const cors = require('cors')
 
 const server = http.createServer(app)
 const io = socketIo(server)
