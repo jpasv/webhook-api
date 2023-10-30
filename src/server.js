cd1358d13aea901d81427e7919b28dcb5449b16b
João Pedro <jp@asv.digital>
2023-10-30 03:56:57 -0300

    update kiwify webhook

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -10,23 +11,22 @@ const io = socketIo(server);
 app.use(cors());
 app.use(express.json());
 
-app.use((err, req, res, next) => {
-  if (err) {
-    console.error(err.stack);
-    res.status(400).send('Error: ' + err.message);
-    return;
-  }
-  next();
+app.all('/webhook', bodyParser.raw({ type: 'application/json' }), (req, res) => {
+	if (req.method !== 'POST') {
+		return res.status(200).send({ status: 'ok' });
+	}
+
+	let order = {};
+	try {
+		order = JSON.parse(req.body);
+	} catch (error) {
+		return res.status(400).send({ error });
+	}
+
+	console.log('Received order:', order);
+
+	return res.status(200).send({ status: 'ok' });
 });
-
-let dataReceived = [];
-
-
-app.post('/webhook', async (req, res) => {
-    console.log(req.body);
-    dataReceived.push(req.body);
-    res.status(200).send('Dados recebidos!');
-  });
 
 app.get('/data', async (req, res) => {
     res.status(200).json(dataReceived);
