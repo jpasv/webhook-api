01f06ff0399d454ad53a76b62c14a3733d4f2165
João Pedro <jp@asv.digital>
2023-10-30 04:13:59 -0300

    pos

--- src/server.js
+++ src/server.js
@@ -5,23 +5,23 @@ const app = express();
 
 app.use(express.json());
 
-let order = {};
+let dataReceived = [];
 
-app.all('/webhook', async (req, res) => {
+app.post('/webhook', (req, res) => {
     console.log('Cabeçalhos:', req.headers);
     console.log('Corpo:', req.body);
 
-    if (!req.body) {
-        console.log('400 Error: Corpo da requisição vazio');
-        return res.status(400).send('Corpo da requisição vazio');
+    if (!req.body || Object.keys(req.body).length === 0) {
+        return res.status(400).send('Corpo da solicitação vazio ou inválido');
     }
-    
-    order = req.body;
-    res.status(200).json({ message: 'ok' })
-})
+
+    dataReceived.push(req.body);
+    res.status(200).send('Dados recebidos!');
+});
+
 
 app.get('/data', async (req, res) => {
-    res.status(200).json(order);
+    res.status(200).json(dataReceived);
 });
 
 app.listen(3000, () => {
